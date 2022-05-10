d5395c83d2b5580f4a565e314236d920aade5e8a
Chris Lacey <clacey@FAMACIQ4J92K.local>
2022-05-10 13:26:48 -0400

    Exercise 1

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -23,15 +23,21 @@ const SuperHeader = () => {
 };
 
 const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  height: 40px;
+  padding-left: 32px;
+  padding-right: 32px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-right: auto;
 `;
-
 const HelpLink = styled.a`
   color: inherit;
   text-decoration: none;
