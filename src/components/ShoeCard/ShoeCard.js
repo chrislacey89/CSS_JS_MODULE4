764b559c56dfc2a30e5dd79f9775b37eeaa0c043
Chris Lacey <clacey@FAMACIQ4J92K.local>
2022-05-20 12:53:03 -0400

    Part 4A

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -53,6 +53,7 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex: 1;
 `;
 
 const Wrapper = styled.article``;
@@ -61,7 +62,9 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
